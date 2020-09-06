05502748f11fbfbf1943b45a4abbbcd24b8a62ad
Shubham Swami <shubhamswami2795@gmail.com>
2020-09-05 18:21:18 -0600

    Styling and features added, some more functionalities remain

--- springBoot/covidTracker/src/main/java/UdemyCourse/springBoot/covidTracker/controller/HomeController.java
+++ springBoot/covidTracker/src/main/java/UdemyCourse/springBoot/covidTracker/controller/HomeController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Date;
 import java.util.List;
 
 @Controller
@@ -16,10 +17,14 @@ public class HomeController {
 
     @GetMapping("/") // whenever there is a mapping to the root url and we've added hymeleaf as a dependency
      public String home(Model model){
+
+
+        Date date = new Date();
         List<LocationStatistics> allStats = covidDataService.getStatisticsList();
         int totalGlobalCases =  allStats.stream().mapToInt(stat -> stat.getTotalCases()).sum();
         model.addAttribute("locationStatistics",covidDataService.getStatisticsList());
         model.addAttribute("totalGlobalCases",totalGlobalCases);
+        model.addAttribute("date",date);
         return "home";
      }
 }
